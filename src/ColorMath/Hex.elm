0cc3d0221e31ab7e320695e42e9dc9b86e673b96
Daniel C Narey <danielnarey@users.noreply.github.com>
2018-01-05 10:27:18 -0500

    bump to 2.0.0
    - reconfigured package structure
    - simplified hex-to-color algorithm
    - separated hex-6 and hex-8 functions
    - improved documentation

--- src/ColorMath/Hex.elm
+++ src/ColorMath/Hex.elm
@@ -2,47 +2,67 @@ module ColorMath.Hex exposing
   ( fromInt, fromColor, toColor )
 
 {-|
+
+# Converting To and From Hexadecimal Color Codes
+- Hex codes are widely used in CSS, but are not currently supported by the
+*elm-lang/core* `Color` module
+- This module provides a convenient way to convert hex codes (e.g., for
+additional color processing within an Elm program), or to output color
+identifiers in a compact format (e.g., when creating a color tool that would
+allow users to cut and paste color codes). 
+
 @docs fromInt, fromColor, toColor
 
 -}
 
-import Array
+import Toolkit.Function
+import Toolkit.Tuple
+import Toolkit.Tuple3
+import Toolkit.List
+import Toolkit.Maybe
+import Toolkit.Result
 
-{-|
+import Color exposing (Color)
+import Array
+import Char
+
+
+{-| Given an integer between 0 and 255, returns its equivalent in base-16
+notation
+
+  - Negative input returns "00"
+  - Input greater than 255 returns "FF"
 
 -}
 fromInt : Int -> String
 fromInt int =
   let
-    base16Digits =
+    toBase16 int =
       [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
       , "A", "B", "C", "D", "E", "F"
       ]
         |> Array.fromList
+        |> Array.get int
+        |> Maybe.withDefault "!"
 
     clamped =
       int
         |> clamp 0 255
 
   in
-    ( base16Digits
-      |> Array.get (clamped // 16)
-      |> Maybe.withDefault "!"
-
-    , base16Digits
-      |> Array.get (rem clamped 16)
-      |> Maybe.withDefault "!"
-
+    ( clamped // 16
+    , rem clamped 16
     )
+      |> Toolkit.Tuple.map toBase16
       |> uncurry String.append
 
 
-{-| Given a `Color`, returns the equivalent hexademimal color code as a
+{-| Given a `Color`, returns its equivalent hexademimal color code as a
 6-character string, with the alpha channel ignored
 -}
 fromColor : Color -> String
 fromColor =
-  Color.rgb
+  Color.toRgb
     >> Toolkit.Function.applyList
       [ .red
       , .green
@@ -52,124 +72,94 @@ fromColor =
     >> String.concat
 
 
+{-| Converts a string containing a 3- or 6-digit hexadecimal color code
+to a `Color`
 
+- Returns an error message if the string argument is not valid
+hexadecimal notation, or contains an incorrect number of digits.
+- The hex code may be given with or without a leading "#".
 
--- -- CONVERTING HEXADECIMAL CODES TO `Color` VALUES
---
--- {-| Converts a string containing a 3-, 4-, 6-, or 8-digit hexadecimal color code
--- to a `Color`. Returns an error message if the string argument is not a valid
--- hexadecimal code. The hex code may be given with or without a leading "#".
--- -}
--- hexToColor : String -> Result String Color
--- hexToColor hexCode =
---   let
---     dropFirstChar charList =
---       if (charList |> List.head) == Just '#' then
---         charList
---           |> List.drop 1
---
---       else
---         charList
---
---     checkDigits charList =
---       case charList |> List.length of
---         3 ->
---           charList
---            .|> (\c -> [c, c])
---             |> List.concat
---             |> List.map charToNum
---             |> Helpers.resultList errorMsg
---            !+> separateRgb
---            !|> Helpers.map3Tuple hexToInt
---            !|> (Color.rgb |> Helpers.uncurry3)
---
---         4 ->
---           charList
---            .|> (\c -> [c, c])
---             |> List.concat
---             |> List.map charToNum
---             |> Helpers.resultList errorMsg
---             !+> separateRgba
---             !|> Helpers.map4Tuple hexToInt
---             !|> normalizeAlpha
---             !|> (Color.rgba |> Helpers.uncurry4)
---
---         6 ->
---           charList
---             |> List.map charToNum
---             |> Helpers.resultList errorMsg
---             !+> separateRgb
---             !|> Helpers.map3Tuple hexToInt
---             !|> (Color.rgb |> Helpers.uncurry3)
---
---         8 ->
---           charList
---             |> List.map charToNum
---             |> Helpers.resultList errorMsg
---            !+> separateRgba
---            !|> Helpers.map4Tuple hexToInt
---            !|> normalizeAlpha
---            !|> (Color.rgba |> Helpers.uncurry4)
---
---         _ ->
---           errorMsg
---             |> Err
---
---     charToNum char =
---       if char |> Char.isDigit then
---         char
---           |> String.fromChar
---           |> String.toInt
---
---       else
---         [ ('A', 10)
---         , ('B', 11)
---         , ('C', 12)
---         , ('D', 13)
---         , ('E', 14)
---         , ('F', 15)
---         ]
---           |> Dict.fromList
---           |> Dict.get char
---           |> Result.fromMaybe errorMsg
---
---     separateRgb numList =
---       numList
---         |> Helpers.apply3
---           ( Helpers.take2Tuple
---           , List.drop 2 >> Helpers.take2Tuple
---           , List.drop 4 >> Helpers.take2Tuple
---           )
---         |> Helpers.maybe3Tuple
---         |> Result.fromMaybe errorMsg
---
---     separateRgba numList =
---       numList
---         |> Helpers.apply4
---           ( Helpers.take2Tuple
---           , List.drop 2 >> Helpers.take2Tuple
---           , List.drop 4 >> Helpers.take2Tuple
---           , List.drop 6 >> Helpers.take2Tuple
---           )
---         |> Helpers.maybe4Tuple
---         |> Result.fromMaybe errorMsg
---
---     hexToInt (firstDigit, secondDigit) =
---       firstDigit
---         |> (*) 16
---         |> (+) secondDigit
---
---     normalizeAlpha (r, g, b, a) =
---       (r, g, b, toFloat a / 255)
---
---     errorMsg =
---       "Argument is not a valid hexadecimal color code"
---
---   in
---     hexCode
---       |> String.toUpper
---       |> String.toList
---       |> dropFirstChar
---       |> checkDigits
---
---
+-}
+toColor : String -> Result String Color
+toColor hexCode =
+  let
+    dropFirstChar charList =
+      case (charList |> List.head) of
+        Just '#' ->
+          charList
+            |> List.drop 1
+
+        _ ->
+          charList
+
+    checkDigits charList =
+      case (charList |> List.length) of
+        3 ->
+          charList
+            |> List.map (\c -> [c, c])
+            |> List.concat
+            |> Just
+
+        6 ->
+          Just charList
+
+        _ ->
+          Nothing
+
+    fromBase16 keyCode =
+      [ 10, 11, 12, 13, 14, 15 ]
+        |> Array.fromList
+        |> Array.get (keyCode - 65)
+
+    charToInt char =
+      case (char |> Char.isDigit) of
+        True ->
+          char
+            |> String.fromChar
+            |> String.toInt
+            |> Result.toMaybe
+
+        False ->
+          char
+            |> Char.toCode
+            |> fromBase16
+
+    separateRgb =
+      Toolkit.Function.apply3
+        ( Toolkit.List.take2Tuple
+
+        , List.drop 2
+          >> Toolkit.List.take2Tuple
+
+        , List.drop 4
+          >> Toolkit.List.take2Tuple
+
+        )
+
+        >> Toolkit.Maybe.zip3
+
+    hexToInt =
+      Tuple.mapFirst ((*) 16)
+        >> uncurry (+)
+
+    errorMsg =
+      "'"
+        ++ hexCode
+        ++ "' is not a valid 3- or 6-digit hexadecimal color code"
+
+  in
+    hexCode
+      |> String.toUpper
+      |> String.toList
+      |> dropFirstChar
+      |> checkDigits
+      |> Maybe.andThen
+        ( List.map charToInt
+          >> Toolkit.Maybe.zipList
+        )
+      |> Maybe.andThen separateRgb
+      |> Maybe.map
+        ( Toolkit.Tuple3.map hexToInt
+          >> Toolkit.Function.uncurry3 Color.rgb
+        )
+      |> Result.fromMaybe errorMsg
--- src/ColorMath/Hex8.elm
+++ src/ColorMath/Hex8.elm
@@ -1,22 +1,156 @@
 module ColorMath.Hex8 exposing
   ( fromColor, toColor )
 
+{-|
+
+# Converting To and From 8-Digit Hexadecimal Color Codes
+- 8-digit color codes include a hexadecimal encoding of the alpha channel
+- Just like typical color codes, they can be abbreviated to 4 digits so that
+"#FFFFFFFF" becomes "#FFFF"
+- 8-digit codes are supported in Firefox and Chrome > 52
+- Read about pros and cons [here](https://css-tricks.com/8-digit-hex-codes/)
+
+@docs fromColor, toColor
+
+-}
+
+import ColorMath.Hex
+
+import Toolkit.Function
+import Toolkit.Tuple
+import Toolkit.Tuple4
+import Toolkit.List
+import Toolkit.Maybe
+import Toolkit.Result
+
+import Color exposing (Color)
+import Array
+import Char
+
 
 {-| Given a `Color`, returns the equivalent hexademimal color code as an
 8-character string, with the alpha channel represented by the last two
-characters (note that 8-digit hex codes are not yet supported by all browsers).
+characters
 -}
 fromColor : Color -> String
 fromColor =
-  Toolkit.Function.apply2
-    ( ColorMath.Hex.fromColor
-    , ColorMath.Rgb.getAlpha
-      >> (*) 255
-      >> round
-      >> ColorMath.Hex.fromInt
+  Color.toRgb
+    >> Toolkit.Function.applyList
+      [ .red
+      , .green
+      , .blue
+      , .alpha
+        >> (*) 255
+        >> round
 
-    )
-      >> uncurry String.append
+      ]
+
+    >> List.map ColorMath.Hex.fromInt
+    >> String.concat
 
 
+{-| Converts a string containing a 4- or 8-digit hexadecimal color code
+to a `Color`
+
+- Returns an error message if the string argument is not valid
+hexadecimal notation, or contains an incorrect number of digits.
+- The hex code may be given with or without a leading "#".
+
+-}
 toColor : String -> Result String Color
+toColor hexCode =
+    let
+      dropFirstChar charList =
+        case (charList |> List.head) of
+          Just '#' ->
+            charList
+              |> List.drop 1
+
+          _ ->
+            charList
+
+      checkDigits charList =
+        case (charList |> List.length) of
+          4 ->
+            charList
+              |> List.map (\c -> [c, c])
+              |> List.concat
+              |> Just
+
+          8 ->
+            Just charList
+
+          _ ->
+            Nothing
+
+      fromBase16 keyCode =
+        [ 10, 11, 12, 13, 14, 15 ]
+          |> Array.fromList
+          |> Array.get (keyCode - 65)
+
+      charToInt char =
+        case (char |> Char.isDigit) of
+          True ->
+            char
+              |> String.fromChar
+              |> String.toInt
+              |> Result.toMaybe
+
+          False ->
+            char
+              |> Char.toCode
+              |> fromBase16
+
+      separateRgb =
+        Toolkit.Function.apply4
+          ( Toolkit.List.take2Tuple
+
+          , List.drop 2
+            >> Toolkit.List.take2Tuple
+
+          , List.drop 4
+            >> Toolkit.List.take2Tuple
+
+          , List.drop 6
+            >> Toolkit.List.take2Tuple
+
+          )
+
+          >> Toolkit.Maybe.zip4
+
+      hexToInt =
+        Tuple.mapFirst ((*) 16)
+          >> uncurry (+)
+
+      normalizeAlpha (r, g, b, a) =
+        ( r
+        , g
+        , b
+        , a
+          |> toFloat
+          |> flip (/) 255
+
+        )
+
+      errorMsg =
+        "'"
+          ++ hexCode
+          ++ "' is not a valid 4- or 8-digit hexadecimal color code"
+
+    in
+      hexCode
+        |> String.toUpper
+        |> String.toList
+        |> dropFirstChar
+        |> checkDigits
+        |> Maybe.andThen
+          ( List.map charToInt
+            >> Toolkit.Maybe.zipList
+          )
+        |> Maybe.andThen separateRgb
+        |> Maybe.map
+          ( Toolkit.Tuple4.map hexToInt
+            >> normalizeAlpha
+            >> Toolkit.Function.uncurry4 Color.rgba
+          )
+        |> Result.fromMaybe errorMsg
